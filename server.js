f7147993584e932e39846551e7f22a42c72ed630
Sun Davey <sundavey@hotmail.com>
2018-05-03 16:56:37 -0700

    Real Time color changing app using socket

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+var bodyParser = require('body-parser');
+app.unsubscribe(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname + "./static")));
+app.set('views', path.join(__dirname + "/views"));
+
+app.set('view engine', 'ejs');
+
+const server = app.listen(8000);
+const io = require('socket.io')(server);
+
+io.on('connection', function (socket) { //2
+    
+    socket.on('color', function(data){
+        io.emit('broad', data);
+    })
+
+    // socket.on('reset', function(data){
+    //     counter = 0;
+    //     socket.emit('resetted', counter );
+    // })
+});
+
+
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+
+
